8d69a4390122322f7bf30ce229412542585b0ee1
ledung76d <77008334+ledung76d@users.noreply.github.com>
2021-06-24 17:11:33 +0700

    Delete graph directory

--- graph/thuvien1/coloring.h
+++
@@ -1,7 +0,0 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-void tomau(int *dinh, int *mau, int sodinh,int socanh);
-void xuatfile(int *dinh, int *mau,int sodinh, int socanh,char *filename);
-void coloring_graph(char *filename);
